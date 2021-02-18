5d63a2efef3f97ff7afa0f4d48f696c7d44e07c5
Tobils <ade.suhada@mail.ugm.ac.id>
2021-02-18 08:05:12 +0700

    add product list

--- App.js
+++ App.js
@@ -1,13 +1,119 @@
-import React from 'react';
-import {Image, ScrollView, Text, TextInput, View} from 'react-native'
-import {AppBasic} from './AppBasicComponent'
+import React, {Component} from 'react';
+import {Image, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native'
+import {BasicApp} from './AppBasicComponent'
+import jam from './image-ss/image-watch.jpg'
+
 const App = () => {
   return (
-    <View>
-      <AppBasic></AppBasic>
+    <View style={{ margin: 20 }}>
+      <Text style={style.title}>
+        Product List
+      </Text>
+      <ScrollView horizontal>
+        <ProductComponent></ProductComponent>
+        <ProductComponent></ProductComponent>
+        <ProductComponent></ProductComponent>
+        <ProductComponent></ProductComponent>
+        <ProductComponent></ProductComponent>
+        <ProductComponent></ProductComponent>
+      </ScrollView>
     </View>
+
   )
 };
+
+
+const ProductComponent = () => {
+  return(
+    <View style={style.card}>
+      <Image
+        style={style.productImage}
+        source={ jam }
+      ></Image>
+      <Text style={style.title}>
+        New Smart Watch
+      </Text>
+      <Text style={style.price}>Rp. 6.000.000</Text>
+      <Text style={style.address}>Jakarta Barat</Text>
+      <View style={style.btnCard}>
+        <Text style={style.btnText}>Beli</Text>
+      </View>
+    </View>
+  );
+}
+
+
+
+const style = StyleSheet.create({
+
+    card: {
+      width: 212,
+      borderRadius: 8,
+      backgroundColor: '#dfe4ea',
+      padding: 12,
+      alignContent: 'center',
+      margin: 5
+    },
+    productImage: {
+      height: 107,
+      width: 188,
+      borderRadius: 8,
+      alignContent:'center'
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginTop: 16
+    },
+    price: {
+      fontSize:  14,
+      color:'#ffa502',
+      marginTop: 16,
+      fontWeight: 'bold'
+    },
+    address:{
+      fontSize: 16,
+      fontWeight:'bold',
+      marginTop: 12
+    },
+    btnCard: {
+      backgroundColor: '#70a1ff',
+      paddingVertical: 6,
+      borderRadius: 25,
+      marginTop: 20
+    },
+    btnText: {
+      fontSize: 16,
+      textAlign: 'center',
+      color: 'white',
+      fontWeight: '600'
+    }
+});
+
+
+const SampleComponent = () => {
+  return (
+    <View style={{ margin: 10 }}>
+      <View
+      style={{
+        width:80, 
+        height: 80,
+        backgroundColor: '#81ecec',
+        marginTop: 20
+      }}
+      ></View>
+      <ComponentTobil></ComponentTobil>
+      <Photo></Photo>
+      <TextInput style={{borderWidth: 1}}>
+
+      </TextInput>
+
+      <BoxGreen></BoxGreen>
+      <Profile></Profile>
+
+    </View>
+  );
+}
 
 
 const ComponentTobil = () => {
@@ -28,6 +134,35 @@ const Photo = () => {
       source={require("./image-ss/Bracket-pair.png")}
     ></Image>
   );
+}
+
+class BoxGreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>
+          Box Green
+        </Text>
+      </View>
+    );
+  }
+}
+
+
+class Profile extends Component {
+  render(){
+    return(
+        <View>
+          <Image
+            source={require("./image-ss/Bracket-pair.png")}
+            style={{width:100, height:100,borderRadius:50, marginLeft:10}}
+          ></Image>
+          <Text>
+            bracket pair extension
+          </Text>
+        </View>
+    );
+  }
 }
 
 export default App;
--- AppBasicComponent.js
+++ AppBasicComponent.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Image, ScrollView, Text, TextInput, View} from 'react-native'
 
-const AppBasic = () => {
-  return (
-    <View>
-      <View
-      style={{width:80, height: 80, backgroundColor: '#81ecec'}}
-      ></View>
-      <ComponentTobil></ComponentTobil>
-      <Photo></Photo>
-      <TextInput style={{borderWidth: 1}}>
 
-      </TextInput>
-    </View>
-  )
-};
+class BasicApp extends Component {
+    render(){
+        return (
+            <View>
+                <View
+                style={{width:80, height: 80, backgroundColor: '#81ecec'}}
+                ></View>
+                <ComponentTobil></ComponentTobil>
+                <Photo></Photo>
+                <TextInput style={{borderWidth: 1}}>
+
+                </TextInput>
+            </View>
+        );
+    }
+}
 
 
 const ComponentTobil = () => {
-  return (
-    <ScrollView>
-    <View>
-      <Text>Yuhuuu Mantaff</Text>
-      <Text>Hi</Text>
-    </View>
-  </ScrollView>
-  );
-}
+    return (
+      <ScrollView>
+      <View>
+        <Text>Yuhuuu Mantaff</Text>
+        <Text>Hi</Text>
+      </View>
+    </ScrollView>
+    );
+  }
+  
+  
+  const Photo = () => {
+    return (
+      <Image
+        source={require("./image-ss/Bracket-pair.png")}
+      ></Image>
+    );
+  }
 
-
-const Photo = () => {
-  return (
-    <Image
-      source={require("./image-ss/Bracket-pair.png")}
-    ></Image>
-  );
-}
-
-export default AppBasic;
+export default BasicApp;
